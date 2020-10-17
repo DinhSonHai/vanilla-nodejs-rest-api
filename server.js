49867964ce95c63925bddc77f0aec559ac3632ef
DinhSonHai <sdinhhai@gmail.com>
2020-10-17 22:37:59 +0700

    Handle route not exist

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ const server = http.createServer((req, res) => {
   // res.setHeader('Content-Type', 'text/html');
   // res.write('<h1>Hello Vanilla Nodejs</h1>');
   // res.end()
-  
-  if(req.url === '/api/products') {
+
+  if(req.url === '/api/products' && req.method === 'GET') {
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify(products))
+  }
+  else {
+    res.writeHead(404, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({ msg: 'Route not found'}))
   }
 })
 
