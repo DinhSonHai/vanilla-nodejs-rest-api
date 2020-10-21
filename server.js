9218fcaf0756e8ae16eed548f383390d9b57ad63
DinhSonHai <sdinhhai@gmail.com>
2020-10-21 21:50:07 +0700

    Create update product route

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const { getProducts, getProductById, createProduct } = require('./controllers/productController')
+const { getProducts, getProductById, createProduct, updateProduct } = require('./controllers/productController')
 
 const server = http.createServer((req, res) => {
   // res.statusCode = 200;
@@ -16,6 +16,10 @@ const server = http.createServer((req, res) => {
   }
   else if (req.url === '/api/products' && req.method === 'POST'){
     createProduct(req, res);
+  }
+  else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT') {
+    const id = req.url.split('/')[3];
+    updateProduct(req, res, id);
   }
   else {
     res.writeHead(404, {'Content-Type': 'application/json'});
