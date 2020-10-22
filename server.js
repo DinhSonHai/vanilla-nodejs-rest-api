fa0631d2c720d00f5791bcac44cd499883cf6b6c
DinhSonHai <sdinhhai@gmail.com>
2020-10-22 20:36:18 +0700

    Add delete product route

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const { getProducts, getProductById, createProduct, updateProduct } = require('./controllers/productController')
+const { getProducts, getProductById, createProduct, updateProduct, deleteProduct } = require('./controllers/productController')
 
 const server = http.createServer((req, res) => {
   // res.statusCode = 200;
@@ -20,6 +20,10 @@ const server = http.createServer((req, res) => {
   else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT') {
     const id = req.url.split('/')[3];
     updateProduct(req, res, id);
+  }
+  else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE') {
+    const id = req.url.split('/')[3];
+    deleteProduct(req, res, id);
   }
   else {
     res.writeHead(404, {'Content-Type': 'application/json'});
